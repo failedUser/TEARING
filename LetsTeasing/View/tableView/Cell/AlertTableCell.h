e88d68da02964ddeb01112144d2b01e024fb2afb
apple <yyasyya@163.com>
2016-07-25 17:12:47 +0800

    UI second

---
+++ LetsTeasing/View/tableView/Cell/AlertTableCell.h
@@ -0,0 +1,13 @@
+//
+//  AlertTableCell.h
+//  LetsTeasing
+//
+//  Created by apple on 16/7/25.
+//  Copyright © 2016年 yueyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlertTableCell : UITableViewCell
+
+@end
---
+++ LetsTeasing/View/tableView/Cell/AlertTableCell.m
@@ -0,0 +1,24 @@
+//
+//  AlertTableCell.m
+//  LetsTeasing
+//
+//  Created by apple on 16/7/25.
+//  Copyright © 2016年 yueyin. All rights reserved.
+//
+
+#import "AlertTableCell.h"
+
+@implementation AlertTableCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
